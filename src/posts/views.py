7c78a44f0d9347bafee30202c8556da92061b146
mayank singh <mayanksingh8005@gmail.com>
2018-01-22 14:44:48 +0530

    Update views.py

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 # Create your views here.
@@ -10,6 +10,7 @@ def post_create(request):
 		instance =form.save(commit=False)
 		print form.cleaned_data.get("title")
 		instance.save()
+		return HttpResonseRedirect(instance.get_absolute_url())
 	#if request.method == "POST":
 		#print "title" + request.POST.get("content")
 		#print request.POST.get("title")
@@ -43,7 +44,20 @@ def post_list(request):# items in the list
 	}
 	return render(request,"index.html",context)
 	#return HttpResponse("<h1>List</h1>")
-def post_update(request):
-	return HttpResponse("<h1>Update</h1>")
+def post_update(request, id =None):
+	instance=get_object_or_404(Post,id=id)
+	form=PostForm(request.POST or None, instance=instance)
+	if form.is_valid():
+		instance =form.save(commit=False)
+		instance.save()
+		return HttpResonseRedirect(instance.get_absolute_url())
+	context={
+		"title":instance.title,
+		"instance":instance,
+		"form":form,
+	}
+	return render(request,"post_form.html",context)
+
+	
 def post_delete(request):
 	return HttpResponse("<h1>Delete</h1>")
