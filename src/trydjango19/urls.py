3940c1dc381e3d5b451582b821fd8f24227da8e8
mayank singh rajkumar <mayanksingh8005@gmail.com>
2018-02-24 23:51:47 +0530

    Update urls.py

--- src/trydjango19/urls.py
+++ src/trydjango19/urls.py
@@ -1,5 +1,5 @@
-"""trydjango19 URL Configuration
 
+"""trydjango19 URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
 Examples:
@@ -15,15 +15,16 @@ Including another URLconf
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf import settings
-from django.conf.urls import include , url
+from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^admin/', admin.site.urls),
-    url(r'^posts/',include("posts.urls",namespace='posts')),
-    #url(r'^posts/$',"<appname>.views.<function_name>"),
+    url(r'^admin/', admin.site.urls),
+    url(r'^posts/', include("posts.urls", namespace='posts')),
+    #url(r'^posts/$', "<appname>.views.<function_name>"),
 ]
 
 if settings.DEBUG:
-	urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
